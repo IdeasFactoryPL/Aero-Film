7142b5df7241018807bde6e67e351d411aa19061
Rafał Petryka <rafalpetryka@gmail.com>
2016-02-17 18:43:25 +0100

    Added some new photos and movies

--- js/movie_hover.js
+++ js/movie_hover.js
@@ -1,17 +1,17 @@
 $(window).load(function(){
-        var figure = $(".video");
-        var vid = $("video");
+  var figure = $(".video");
+  var vid = $("video");
 
-        [].forEach.call(figure, function (item) {
-                item.addEventListener('mouseover', hoverVideo, false);
-                item.addEventListener('mouseout', hideVideo, false);
-        });
-        
-        function hoverVideo(e) {  
-                $(this).find('.thevideo')[0].play();
-        }
+  [].forEach.call(figure, function (item) {
+    item.addEventListener('mouseover', hoverVideo, false);
+    item.addEventListener('mouseout', hideVideo, false);
+  });
 
-        function hideVideo(e) {
-                $(this).find('.thevideo')[0].pause(); 
-        }
+  function hoverVideo(e) {  
+    $(this).find('.thevideo')[0].play();
+  }
+
+  function hideVideo(e) {
+    $(this).find('.thevideo')[0].pause(); 
+  }
 });
